c14f54ca982d875f28eda9ddb9dc51144a94063f
osawa-koki <github@osawa.dev>
2022-12-08 23:16:12 +0900

    ES記法に。

--- next.config.js
+++ next.config.js
@@ -4,7 +4,4 @@ const SUB_DIRECTORY = "/simple-next-study.ssg.ts";
 /* 本番環境と開発環境の分岐用のフラグ */
 const isProd = process.env.NODE_ENV === "production";
 
-module.exports = {
-  // 本番環境では SUB_DIRECTORY それ以外は空欄
-  assetPrefix: isProd ? SUB_DIRECTORY : "",
-};
+export const assetPrefix = isProd ? SUB_DIRECTORY : "";
