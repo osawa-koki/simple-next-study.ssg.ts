b656148635a9235330970e8c069e21316dc9ef4f
osawa-koki <github@osawa.dev>
2023-01-01 21:49:21 +0900

    pageコピーした。

---
+++ pages/reference.jsx
@@ -0,0 +1,18 @@
+import PageBlock from "../components/pages";
+
+export default function HelloWorld() {
+  return (
+    <main>
+      <div id="Ref">
+        <h1>Here,  About page.</h1>
+        <p>学習には以下のサイトをオススメします。</p>
+        <ul>
+          <li><a href="https://nextjs.org/learn/basics/create-nextjs-app">Next.js公式チュートリアル</a></li>
+          <li><a href="https://react-bootstrap.github.io/">React Bootstrap公式ドキュメント</a></li>
+        </ul>
+        <hr />
+        <PageBlock />
+      </div>
+    </main>
+  );
+};
